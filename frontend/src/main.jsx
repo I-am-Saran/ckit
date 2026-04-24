151cb892758ac7bdcce29e14ed0481090f6f97c7
Saranraj S <saran.s@hepl.com>
2026-04-24 17:25:25 +0530

    font change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/dashboard.scss';
+import './index.css'
 
 const root = createRoot(document.getElementById('root'));
 root.render(
